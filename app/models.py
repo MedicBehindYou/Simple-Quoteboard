79bcc71b7856d48127e781e6b2475a190b91b291
MedicBehindYou <byy0url3ave@gmail.com>
2024-09-18 18:37:28 -0500

    Added is_admin to user model

--- app/models.py
+++ app/models.py
@@ -1,17 +1,21 @@
+# models.py
 from app import db, bcrypt
 from flask_login import UserMixin
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
-    username = db.Column(db.String(150), unique=True, nullable=False)  # New field for profile name
+    username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)  # New field for admin status
 
     def set_password(self, password):
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
+
 
 
 
