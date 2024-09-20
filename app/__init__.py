e3f5f1f68b91d07ddb4210980a4250dd7e70ebd7
MedicBehindYou <byy0url3ave@gmail.com>
2024-09-19 19:53:51 -0500

    Adding flask migrate to assist with db changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
+#__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = 'main.login' 
 
@@ -18,6 +21,7 @@ def create_app():
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    migrate.init_app(app, db)
 
     from app.routes import bp as main_bp
     app.register_blueprint(main_bp)
